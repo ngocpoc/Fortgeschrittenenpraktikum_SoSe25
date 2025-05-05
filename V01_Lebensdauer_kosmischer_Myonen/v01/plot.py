0f975100cbf2bfe3c6fdd2c04ea53bfbaf4e689e
Amelie Hater <amelie.hater@tu-dortmund.de>
2025-05-05 21:08:58 +0200

    Verschönerung der Plots und letzter Graph

--- V01_Lebensdauer_kosmischer_Myonen/v01/plot.py
+++ V01_Lebensdauer_kosmischer_Myonen/v01/plot.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
+from scipy.optimize import curve_fit
 
 delay, Anzahl_Delay_pro_30_s = np.genfromtxt("Messdaten/Variation_Verzoegerungsleitung.txt", unpack=True)
 
@@ -49,13 +50,13 @@ ax.plot(delay,Anzahl_Delay_pro_1_s, "o", mfc='none', color='steelblue')
 #)
 #plt.grid()
 #plt.plot (-5, Anzahl_Delay_pro_1_s, "x", label = "Messwerte")
-ax.hlines(y=max(Anzahl_Delay_pro_1_s)/2, xmin=-5, xmax=7, linewidth=2, color='r', linestyle = "dashed")
-ax.hlines(y=params[0], xmin=-1.5, xmax=3.5, linewidth=2, color='r', linestyle = "solid")
+ax.hlines(y=max(Anzahl_Delay_pro_1_s)/2, xmin=-5, xmax=7, linewidth=2, color='r', linestyle = "dashed", label = "Halbwertsbreite")
+ax.hlines(y=params[0], xmin=-1.5, xmax=3.5, linewidth=2, color='r', linestyle = "solid", label = "Ausgleichskonstante")
 ax.axvline(x=halbwerts_links,ymin = 0, ymax = 0.8, linestyle = "dashed", color="violet")
 ax.axvline(x=halbwerts_rechts,ymin = 0, ymax = 0.8, linestyle = "dashed", color="violet")
 #plt.axhline(y=, color='r', linestyle='-')
 ax.set_xlabel("Verzögerung in ns")
-ax.set_ylabel("Counts per s")
+ax.set_ylabel(r"Anzahl in $1 / \mathrm{s}$")
 ax.legend(loc = "best")
 #plt.margins(0.075)
 fig.savefig("Verzoegerung.pdf")
@@ -77,7 +78,7 @@ print("m ist: ", params1[0])
 x_plot = np.linspace(0,400) 
 
 fig1,ax1 = plt.subplots(figsize = (6,5))
-ax1.plot(Kanalnummer,Zeitdifferenz_in_mikrosek, "x", mfc='none')
+ax1.plot(Kanalnummer,Zeitdifferenz_in_mikrosek, "x", mfc='none', label = "Messwerte")
 plt.plot(
     x_plot,
     params1[0] * x_plot + params1[1],
@@ -85,14 +86,55 @@ plt.plot(
     linewidth=1,
 )
 ax1.set_xlabel("Kanalnummer")
-ax1.set_ylabel("Zeitdifferenz in mikrosek")
+ax1.set_ylabel(r"Zeitdifferenz in $\mu\mathrm{s}$")
 ax1.legend(loc = "best")
 #plt.margins(0.075)
 fig1.savefig("Kalibrierung_MUltichannel.pdf")
 
+#ax.set_xlabel(r"$t / \mathrm{s}$")
+#ax.set_ylabel(r"$U / \mathrm{V}$");
+
+#Berechnung der Lebensdauer der Myonen 
+
+Counts_pro_Channel = np.genfromtxt("Messdaten/Lebendauer_Berechnung.txt", unpack=True)
+channelnumber = np.array(range(1,413))
+
+#print(channelnumber)
+Lebensdauer_Messwerte = params1[0] * channelnumber + params1[1]
 
 
+#print(Lebensdauer_Messwerte)
+Counts_pro_Channel_fit = Counts_pro_Channel[16:]
+Lebensdauer_Messwerte_fit = Lebensdauer_Messwerte[16:]
+print(len(Counts_pro_Channel_fit), len(Lebensdauer_Messwerte_fit))
+def exp(x,a,b,U):
+    return a*np.exp(-b*x) + U
 
+#scipy.optimize.curve_fit lambda x,a,b,U: a*np.exp(-b*x) + U
+params2, covariance_matrix2 = curve_fit(exp,  Lebensdauer_Messwerte_fit,  Counts_pro_Channel_fit, p0 = (2,2,2))
+
+uncertainties = np.sqrt(np.diag(covariance_matrix2))
+
+for name, value, uncertainty in zip("abc", params2, uncertainties):
+    print(f"{name} = {value:8.3f} ± {uncertainty:.3f}")
+
+#a =  306.815 ± 43.820
+#b =    2.762 ± 0.221
+#U =    3.770 ± 0.531
+
+x_plot2 = np.linspace(0.1,9)
+a = params2[0]
+b = params2[1]
+U = params2[2]
+
+fig2,ax2 = plt.subplots(figsize = (6,5))
+ax2.plot(Lebensdauer_Messwerte,Counts_pro_Channel, ".", mfc='none', label = "Messwerte")
+ax2.plot(x_plot2, a*np.exp(-b*x_plot2) + U, label = "Ausgleichskurve" )
+ax2.set_xlabel(r"t in $\mu\mathrm{s}$")
+ax2.set_ylabel("Anzahl")
+ax2.legend(loc = "best")
+#plt.margins(0.075)
+fig2.savefig("Lebensdauer_der_Myonen.pdf")
 
 #params, covariance_matrix = np.polyfit(x, y, deg=0, cov=True)
 
