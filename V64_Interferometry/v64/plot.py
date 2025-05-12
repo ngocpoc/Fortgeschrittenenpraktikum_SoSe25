fc00755d6c228fd810688deb4308bc2a6065ef00
ngocpoc <ngoc-le@hotmail.de>
2025-05-12 15:51:26 +0200

    errors und Auswertung

--- V64_Interferometry/v64/plot.py
+++ V64_Interferometry/v64/plot.py
@@ -1,8 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from uncertainties.unumpy import uarray
 
 def rel_Abweichung(exp, theo):
     return (np.abs(exp-theo)/(theo)*100) #ist schon in Prozent
+
+def contrast(U_max, U_min):
+    return (U_max - U_min)/(U_max + U_min)
+
+def contrast_theo(phi):
+    return np.abs(2*np.cos(phi)*np.sin(phi))
+
+phi, U_max1, U_max2, U_max3, U_min1, U_min2, U_min3= np.genfromtxt("Messdaten/contrast.txt", unpack=True)
+
+# phi = uarray(phi, 1.0)
+# U_max1, U_max2, U_max3 = uarray(U_max1, 0.02), uarray(U_max2, 0.02), uarray(U_max3, 0.02)
+# U_min1, U_min2, U_min3 = uarray(U_min1, 0.02), uarray(U_min2, 0.02), uarray(U_min3, 0.02)
+
+U_max = (U_max1 + U_max2 + U_max3)/ 3
+U_min = (U_min1 + U_min2 + U_min3)/ 3
+
+x = np.linspace(0, np.pi, 1000)
+
+fig, ax = plt.subplots(1, 1, layout="constrained")
+ax.plot(phi, contrast(U_max, U_min), "o", label="Calculated values")
+ax.plot(x * 180 / np.pi, contrast_theo(x), "-", label="Theoretical curve")
+ax.set_xlabel(r"$\phi \mathbin{/} \unit{\degree}$")
+ax.set_ylabel(r"Contrast $\nu$")
+ax.legend(loc="best")
+plt.grid()
+fig.savefig("build/constrast.pdf")
+
 
 # x = np.linspace(0, 10, 1000)
 # y = x ** np.sin(x)
