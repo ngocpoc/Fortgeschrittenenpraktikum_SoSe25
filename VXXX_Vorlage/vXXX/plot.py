6756ac019e5ca9ab079728e17e8e90ed18505922
ngocpoc <ngoc-le@hotmail.de>
2025-06-01 11:54:52 +0200

    Vorlage angepasst

--- VXXX_Vorlage/vXXX/plot.py
+++ VXXX_Vorlage/vXXX/plot.py
@@ -4,18 +4,18 @@ import numpy as np
 def rel_Abweichung(exp, theo):
     return (np.abs(exp-theo)/(theo)*100) #ist schon in Prozent
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+# x = np.linspace(0, 10, 1000)
+# y = x ** np.sin(x)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-ax1.plot(x, y, label="Kurve")
-ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax1.legend(loc="best")
+# fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
+# ax1.plot(x, y, label="Kurve")
+# ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
+# ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+# ax1.legend(loc="best")
 
-ax2.plot(x, y, label="Kurve")
-ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-ax2.legend(loc="best")
+# ax2.plot(x, y, label="Kurve")
+# ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
+# ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
+# ax2.legend(loc="best")
 
-fig.savefig("build/plot.pdf")
+# fig.savefig("build/plot.pdf")
