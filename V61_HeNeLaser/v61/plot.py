1f19b935f8dd2f63abb16fe3f5beca50a0c06502
Amelie Hater <amelie.hater@tu-dortmund.de>
2025-07-14 14:59:22 +0200

    Korrekturen bei V42

--- V61_HeNeLaser/v61/plot.py
+++ V61_HeNeLaser/v61/plot.py
@@ -89,7 +89,7 @@ ax1.set_xlabel("Laenge")
 ax1.set_ylabel("1/Frequenzen")
 ax1.legend(loc="best")
 
-fig.savefig("build/plot.pdf")
+fig.savefig("build/Frequenzspektrum.pdf")
 
 #Ausgabe: 
 #1.Messung:
@@ -133,15 +133,153 @@ fig.savefig("build/plot.pdf")
 # 71.0+/-1.4142135623730951 71.0+/-1.4142135623730951]
 #5.Mittelwert: 72.86+/-0.10
 
+#Moden des Lasers (00 und 01) x = r !!!!
+#Anfang mit 00er Mode 
+
+x_mm_00, I_muA_00 = np.genfromtxt("Messwerte/Mode_00.txt", unpack=True)
+
+def Gaus(x,I_0,x_0,w):
+    return I_0*np.exp(-2 *((x+x_0)**2)/(w**2))
+
+params2, covariance_matrix2 = curve_fit(Gaus,  x_mm_00,  I_muA_00) #p0 = (1000,2,4)
+
+uncertainties2 = np.sqrt(np.diag(covariance_matrix2))
+
+for name, value, uncertainty in zip("Ixw", params2, uncertainties2):
+    print(f"{name} = {value:8.4f} ± {uncertainty:.4f}")  
+
+#I_0 = 13.3068 ± 0.1892  
+#x_0 = -3.9828 ± 0.0791  
+#w =    9.6445 ± 0.1584  
+
+x_plot_2 = np.linspace(-12,20,1000)
+
+fig, (ax1) = plt.subplots(1, 1, layout="constrained")
+#plt.errorbar(Laengen, Mittelwerte_plot, yerr = Mittelwerte_plot_Fehler, xerr = Laengen_Fehler, fmt='o', alpha = 0.4, capsize = 3.3, color = "hotpink", label="Messwerte")
+ax1.plot(
+    x_mm_00,
+    I_muA_00,
+    "x",
+    label="Messwerte",
+    color="blueviolet",
+)
+ax1.plot(
+    x_plot_2,
+    Gaus(x_plot_2,params2[0],params2[1],params2[2]),
+    label="Ausgelichskurve",
+    color="hotpink",
+)
+ax1.set_xlabel("x/mm")
+ax1.set_ylabel("I /muA")
+ax1.legend(loc="best")
+
+fig.savefig("build/Mode_00.pdf")
+
+#Jetzt 01 Mode 
+
+x_mm_01, I_muA_01 = np.genfromtxt("Messwerte/Mode_01.txt", unpack=True)
+
+def Gaus01(x,I_0,x_0,w):
+    return I_0*(((x+x_0)/w)**2)*np.exp(-2 *((x+x_0)**2)/(w**2))
+
+params3, covariance_matrix3 = curve_fit(Gaus01,  x_mm_01,  I_muA_01, p0 = (3.5,2.5,15)) #
+
+uncertainties3 = np.sqrt(np.diag(covariance_matrix3))
+
+for name, value, uncertainty in zip("Ixw", params3, uncertainties3):
+    print(f"{name} = {value:8.4f} ± {uncertainty:.4f}")  
+
+#I_01 = 17.3682 ± 0.4487 
+#x_01 = -2.9582 ± 0.1126 
+#w_01 =  9.7969 ± 0.1565 
+
+x_plot_3 = np.linspace(-14,20,1000)
+
+fig, (ax1) = plt.subplots(1, 1, layout="constrained")
+#plt.errorbar(Laengen, Mittelwerte_plot, yerr = Mittelwerte_plot_Fehler, xerr = Laengen_Fehler, fmt='o', alpha = 0.4, capsize = 3.3, color = "hotpink", label="Messwerte")
+ax1.plot(
+    x_mm_01,
+    I_muA_01,
+    "x",
+    label="Messwerte",
+    color="blueviolet",
+)
+ax1.plot(
+    x_plot_3,
+    Gaus01(x_plot_3,params3[0],params3[1],params3[2]),
+    label="Ausgleichskurve",
+    color="hotpink",
+)
+ax1.set_xlabel("x/mm")
+ax1.set_ylabel("I /muA")
+ax1.legend(loc="best")
+
+fig.savefig("build/Mode_01.pdf")
 
 
+#Polarisation des Lasers RIP ich hasse Polarplots 
 
+theta_Grad, I_muA_Pol = np.genfromtxt("Messwerte/Polarisation.txt", unpack=True)
 
+theta_Rad = np.radians(theta_Grad)
 
+def Malus(theta, theta0, I_0):
+    return I_0 * np.cos((theta - theta0))** 2 
 
+params4, covariance_matrix4 = curve_fit(Malus,  theta_Rad,  I_muA_Pol, p0 = (np.pi/2,7)) #
 
+uncertainties4 = np.sqrt(np.diag(covariance_matrix4))
 
+for name, value, uncertainty in zip("tI", params4, uncertainties4):
+    print(f"{name} = {value:8.4f} ± {uncertainty:.4f}")  
 
+#theta_0_Rad =   1.5043 ± 0.0106
+#I_0 =   5.7701 ± 0.0705
+
+x_Pol_plot = np.linspace(0,2*np.pi, 1000)
+
+fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
+ax.plot(
+    theta_Rad, 
+    I_muA_Pol, 
+    "x", 
+    label = "Messwerte")
+ax.plot(
+    x_Pol_plot,
+    Malus(x_Pol_plot,params4[0],params4[1]),
+    label = "Ausgleichskurve"
+)
+ax.legend(loc="best")
+
+fig.savefig("build/Polarisation.pdf")
+
+#Wellenlänge des Lasers 
+x_mm_Wellen, I_muA_Wellen  = np.genfromtxt("Messwerte/Wellenlaenge.txt", unpack=True)
+
+fig, (ax1) = plt.subplots(1, 1, layout="constrained")
+ax1.plot(
+    x_mm_Wellen,
+    I_muA_Wellen,
+    "x",
+    label="Messwerte",
+    color="blueviolet",
+)
+ax1.set_xlabel("x/mm")
+ax1.set_ylabel("I /muA")
+ax1.legend(loc="best")
+fig.savefig("build/Wellenlaenge.pdf")
+
+d = 0.001 #in cm 10 mumeter (aus anderem Protokoll geklaut)
+L = 20 #in cmeter
+a_1 = 1.05 #in cm 10.5 mmeter
+a_2 = 1.05 #in cm 10.5 mmeter
+n = 1
+#-2              7.56
+#8.5             105.34
+#19              34.41
+lamda_1 = (a_1*d)/(n * L)
+lamda_2 = (a_2*d)/(n * L)
+print("lamda", lamda_1) # lamda = 5.2500000000000006e-05 cm 10^-2 nanometer 10^-9 --> 525 nm 
 
 def rel_Abweichung(exp, theo):
     return (np.abs(exp-theo)/(theo)*100) #ist schon in Prozent
